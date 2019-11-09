af8555d0543d2c7f2d70310b8c0bfb1edc091ff1
Lior Banai <liorbanai@gmail.com>
2019-11-09 21:27:38 +0200

    ininal commit

---
+++ Analogy.DataProviders.Extensions/IAnalogyDataProviderSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analogy.DataProviders.Extensions
+{
+    interface IAnalogyDataProviderSetting
+    {
+        string Title { get; }
+        UserControl DataProviderSettings { get; }
+        System.Drawing.Image Icon { get; } 
+    }
+}

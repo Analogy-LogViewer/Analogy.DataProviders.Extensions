426c8efb9648c2387a10327bc541367cfd8e135c
Lior Banai <liorbanai@gmail.com>
2019-11-30 14:27:55 +0200

    update interfaces with base dll

--- Analogy.DataProviders.Extensions.Core/LogParserSettings.cs
+++ Analogy.DataProviders.Extensions.Core/LogParserSettings.cs
@@ -9,25 +9,7 @@ using Newtonsoft.Json;
 
 namespace Analogy.DataProviders.Extensions
 {
-    public interface ILogParserSettings
-    {
-        /// <summary>
-        /// The initial location of the log files.
-        /// </summary>
-        string Directory { get; set; }
-        List<string> SupportedFilesExtensions { get; set; }
-        bool IsConfigured { get; set; }
-        string Splitter { get; set; }
-        string Layout { get; set; }
-        Dictionary<int, AnalogyLogMessagePropertyName> Maps { get; set; }
-        int ValidItemsCount { get; set; }
-        string AsJson();
-        void Configure(string layout, string splitter, List<string> supportedFilesExtension, Dictionary<int, AnalogyLogMessagePropertyName> maps);
-        void AddMap(int index, AnalogyLogMessagePropertyName name);
-        bool CanOpenFile(string fileName);
-        bool CanOpenFiles(IEnumerable<string> fileNames);
-    }
-
+  
     [Serializable]
     public class LogParserSettings : ILogParserSettings
     {

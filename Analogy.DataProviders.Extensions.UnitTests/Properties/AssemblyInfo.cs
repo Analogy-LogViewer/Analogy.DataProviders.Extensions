9b35e4e7ad062598161d42809c9e0d4376131edb
Lior Banai <liorbanai@gmail.com>
2020-03-03 12:12:45 +0200

    update version and proj sdk

--- Analogy.DataProviders.Extensions.UnitTests/Properties/AssemblyInfo.cs
+++ Analogy.DataProviders.Extensions.UnitTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Analogy.DataProviders.Extensions.UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Analogy.DataProviders.Extensions.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("b8dbc878-8544-49a0-9d0b-383aba38665d")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

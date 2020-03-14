f0befcab35d114c6b2134d4d2983d576af399a34
Lior Banai <liorbanai@gmail.com>
2020-03-14 21:19:58 +0200

    related to Analogy-LogViewer/Analogy.LogViewer#113

--- Analogy.DataProviders.Extensions/IAnalogyDataProviderSetting.cs
+++ Analogy.DataProviders.Extensions/IAnalogyDataProviderSetting.cs
@@ -9,7 +9,14 @@ namespace Analogy.DataProviders.Extensions
     {
         string Title { get; }
         UserControl DataProviderSettings { get; }
-        Image Icon { get; }
+        /// <summary>
+        /// 16x16 icon to show in the Analogy UI
+        /// </summary>
+        Image SmallImage { get; }
+        /// <summary>
+        /// 32x32 icon to show in the Analogy UI
+        /// </summary>
+        Image LargeImage { get; }
         /// <summary>
         /// to which Analogy Factory this user setting belong t0
         /// </summary>

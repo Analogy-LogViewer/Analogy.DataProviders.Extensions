fc94562d958f71138738dd4aa9e5a636dd3ef308
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-15 19:28:55 +0300

    update interface

--- Analogy.DataProviders.Extensions/IAnalogyComponentImages.cs
+++ Analogy.DataProviders.Extensions/IAnalogyComponentImages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,11 @@ namespace Analogy.DataProviders.Extensions
     public interface IAnalogyComponentImages
     {
         IEnumerable<DataProviderImages> GetDataProviderImages();
+        Image GetLargeImage(Guid analogyComponentId);
+        Image GetSmallImage(Guid analogyComponentId);
+        Image GetOnlineConnectedLargeImage(Guid analogyComponentId);
+        Image GetOnlineConnectedSmallImage(Guid analogyComponentId);
+        Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId);
+        Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId);
     }
 }

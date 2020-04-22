fba49367875843d66bf4a290bcad38a93ec7a296
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-22 10:11:23 +0300

    add ExtensionUI

---
+++ Analogy.DataProviders.Extensions/IAnalogyExtensionUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analogy.DataProviders.Extensions
+{
+    interface IAnalogyExtensionUI
+    {
+        /// <summary>
+        /// ID of the ExtensionUI
+        /// </summary>
+        Guid ID { get; }
+        /// <summary>
+        /// ID of the Extension
+        /// </summary>
+        Guid ExtensionID { get; }
+        /// <summary>
+        /// //Optional title to display in the ribbon bar
+        /// </summary>
+        string OptionalTitle { get; }
+        /// <summary>
+        /// The user control to show
+        /// </summary>
+        UserControl UserControl { get; }
+    }
+    }
+
